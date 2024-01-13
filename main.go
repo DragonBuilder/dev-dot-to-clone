e760daab2f91d50e9215b7269a040db56a28c7e4
Aneesh Anil <aneesh@geektrust.in>
2024-01-13 23:35:40 +0530

    moved to go gin framework

--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
-	"log/slog"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
+// func main() {
+// 	r := mux.NewRouter()
+// 	r.HandleFunc("/", IndexHandler).Methods("GET")
+// 	r.HandleFunc("/blog/create", CreateBlogHandler).Methods("GET", "POST")
+
+// 	// http.Handle("/", r)
+
+// 	slog.Info("Starting server on port 8080")
+
+// 	if err := http.ListenAndServe(":8080", r); err != nil {
+// 		slog.Error("Error starting server: %v", err)
+// 	}
+// }
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler).Methods("GET")
-
-	// http.Handle("/", r)
-
-	slog.Info("Starting server on port 8080")
-
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		slog.Error("Error starting server: %v", err)
-	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func CreateBlogHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
 }
